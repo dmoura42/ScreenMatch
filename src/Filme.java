c9ebf742be930c3507373efb6fc0715c094acb28
Daniel Adriano <dmoura.adriano@gmail.com>
2023-05-02 15:25:54 -0300

    Inicio do projeto ScreenMacth

---
+++ src/Filme.java
@@ -0,0 +1,24 @@
+public class Filme {
+    String nome;
+    int anoDeLancamento;
+    boolean incluidoNoPlano;
+    private double somaDasAvaliacoes;
+    private int totalDeAvaliacoes;
+    int duracaoEmMinutos;
+
+    void exibeFichaTecnica() {
+        System.out.println("Nome do filme: " + nome);
+        System.out.println("Ano de lançamento: " + anoDeLancamento);
+        System.out.println("Duração em minutos: " + duracaoEmMinutos);
+        System.out.println("Incluido no plano: " + incluidoNoPlano);
+    }
+
+    void avalia(double nota) {
+        somaDasAvaliacoes += nota;
+        totalDeAvaliacoes++;
+    }
+
+    double pegaMedia() {
+        return somaDasAvaliacoes / totalDeAvaliacoes;
+    }
+}
